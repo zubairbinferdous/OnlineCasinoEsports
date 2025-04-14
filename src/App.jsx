27808c42f44c5e2dc17745801771098118e8339e
SKRJS <zubidev71@gmail.com>
2025-04-14 23:40:41 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 
-import { Link } from 'react-router-dom'
-import './App.css'
-
+import './App.css';
+import Header from './pages/Header';
 function App() {
-
 
   return (
     <>
       <div>
-
-        <ul>
-          <li><a href="/" ></a></li>
-          <li><a href="/about">about</a></li>
-          <li><a href="/home">home</a></li>
-          
-          <Link to={'/about'} > About us </Link>
-        </ul>
+       <Header></Header>
+     
+    
       </div>
    
     </>
